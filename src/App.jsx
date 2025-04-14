bc80c6e7b44824c9b89eb96fa56340ac21837bed
71-husain <154245743+71-husain@users.noreply.github.com>
2025-04-14 09:34:11 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-      <div>
+      <div className="mx-auto px-4 mx-w-screen-xl">
         <Hero/>
         <About/>
         <Projects/>
