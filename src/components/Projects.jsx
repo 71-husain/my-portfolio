ac348bb2075cbfce576320b8e81c8c17da157a47
Husain Ansari <ansarihusain9510@gmail.com>
2026-01-21 05:13:54 -0800

    Add ProjectFlow in projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,45 @@ export default function Projects() {
       <div className="max-w-5xl mx-auto px-6">
         <h2 className="text-3xl font-bold mb-8 text-center">My Projects</h2>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {/* ProjectFlow - Project and task management system*/}
+          <motion.div
+            className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false, amount: 0.3 }}
+            transition={{ duration: 0.6 }}
+          >
+            <img
+              src="/ProjectFlow.png"
+              alt="ProjectFlow"
+              className="w-full h-64 object-cover"
+            />
+            <div className="p-6">
+              <h3 className="text-xl font-semibold mb-2">ProjectFlow</h3>
+              <p className="text-sm text-gray-400 mb-4">
+                Built a full-stack project management system with role-based access (Owner / Member).
+Implemented secure authentication using JWT and protected API routes.
+Designed REST APIs for project, member, and task management workflows.
+Added task assignment, status updates, and member access control features.
+Secured backend using Helmet, restricted CORS, and API rate limiting.
+Deployed frontend on Vercel and backend on Render with production configuration
+              </p>
+             <div className="flex justify-between">
+               <a
+                href="https://github.com/71-husain/projectFlow"
+                className="text-indigo-500 hover:bg-blue-600 hover:text-white p-2 rounded-lg border-2 border-blue-900 outline-blue-900 transition duration-300"
+              >
+                View Project
+              </a>
+              <a
+                href="https://project-flow-lake.vercel.app"
+                className="text-indigo-500 hover:bg-blue-600 hover:text-white p-2 rounded-lg border-2 border-blue-900 outline-blue-900 transition duration-300"
+              >
+                Live Demo
+              </a>
+             </div>
+            </div>
+          </motion.div>
           {/* Social Media -ReelsPro*/}
           <motion.div
             className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
